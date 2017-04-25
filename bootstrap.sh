30d19b3f1dbaf05f9e61a6a4df3577dd8d1e7fc4
Jason Best <jbest@brit.org>
2017-04-25 18:16:38 -0500

    moved most Symbiota-specific configs to symbiota.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -59,20 +59,3 @@ sudo apt-get -y install git
 # install Composer
 # curl -s https://getcomposer.org/installer | php
 # mv composer.phar /usr/local/bin/composer
-
-# install Symbiota files
-sudo git clone https://github.com/Symbiota/Symbiota.git "/var/www/html/${PROJECTFOLDER}"
-# retrieve pre-configured config files
-curl -s https://raw.githubusercontent.com/jbest/symbiota-vagrant/master/config/dbconnection.php > "/var/www/html/${PROJECTFOLDER}/config/dbconnection.php"
-curl -s https://raw.githubusercontent.com/jbest/symbiota-vagrant/master/config/symbini.php > "/var/www/html/${PROJECTFOLDER}/config/symbini.php"
-
-# copy and rename interface template files
-# Perhaps try running /config/setup.sh instead?
-sudo cp /var/www/html/${PROJECTFOLDER}/index_template.php /var/www/html/${PROJECTFOLDER}/index.php
-sudo cp /var/www/html/${PROJECTFOLDER}/leftmenu_template.php /var/www/html/${PROJECTFOLDER}/leftmenu.php
-sudo cp /var/www/html/${PROJECTFOLDER}/header_template.php /var/www/html/${PROJECTFOLDER}/header.php
-sudo cp /var/www/html/${PROJECTFOLDER}/footer_template.php /var/www/html/${PROJECTFOLDER}/footer.php
-sudo cp /var/www/html/${PROJECTFOLDER}/robots_template.txt /var/www/html/${PROJECTFOLDER}/robots.txt
-sudo cp /var/www/html/${PROJECTFOLDER}/css/main_template.css /var/www/html/${PROJECTFOLDER}/css/main.css
-sudo cp /var/www/html/${PROJECTFOLDER}/css/speciesprofile_template.css /var/www/html/${PROJECTFOLDER}/css/speciesprofile.css
-sudo cp /var/www/html/${PROJECTFOLDER}/css/jquery-ui_template.css /var/www/html/${PROJECTFOLDER}/css/jquery-ui.css
--- symbiota.sh
+++ symbiota.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
-#PASSWORD='12345678'
-#PROJECTFOLDER='symbiota'
+PASSWORD='12345678'
+PROJECTFOLDER='symbiota'
+
+# install Symbiota files
+sudo git clone https://github.com/Symbiota/Symbiota.git "/var/www/html/${PROJECTFOLDER}"
+# retrieve pre-configured config files
+curl -s https://raw.githubusercontent.com/jbest/symbiota-vagrant/master/config/dbconnection.php > "/var/www/html/${PROJECTFOLDER}/config/dbconnection.php"
+curl -s https://raw.githubusercontent.com/jbest/symbiota-vagrant/master/config/symbini.php > "/var/www/html/${PROJECTFOLDER}/config/symbini.php"
+
+# copy and rename interface template files
+# Perhaps try running /config/setup.sh instead?
+sudo cp /var/www/html/${PROJECTFOLDER}/index_template.php /var/www/html/${PROJECTFOLDER}/index.php
+sudo cp /var/www/html/${PROJECTFOLDER}/leftmenu_template.php /var/www/html/${PROJECTFOLDER}/leftmenu.php
+sudo cp /var/www/html/${PROJECTFOLDER}/header_template.php /var/www/html/${PROJECTFOLDER}/header.php
+sudo cp /var/www/html/${PROJECTFOLDER}/footer_template.php /var/www/html/${PROJECTFOLDER}/footer.php
+sudo cp /var/www/html/${PROJECTFOLDER}/robots_template.txt /var/www/html/${PROJECTFOLDER}/robots.txt
+sudo cp /var/www/html/${PROJECTFOLDER}/css/main_template.css /var/www/html/${PROJECTFOLDER}/css/main.css
+sudo cp /var/www/html/${PROJECTFOLDER}/css/speciesprofile_template.css /var/www/html/${PROJECTFOLDER}/css/speciesprofile.css
+sudo cp /var/www/html/${PROJECTFOLDER}/css/jquery-ui_template.css /var/www/html/${PROJECTFOLDER}/css/jquery-ui.css
 
 # Create Symbiota database
 mysql -uroot -p12345678 -e "create database symbiota"
